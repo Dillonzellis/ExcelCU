7e0efcaf7a77c48582e0b917bb5e659a03b87008
Dillon Ellis <dellis@fcul.com>
2022-08-05 16:05:18 -0400

    original files after network solutions reset

---
+++ notification-bar/notif.js
@@ -0,0 +1,21 @@
+// const notifBanner1 = document.getElementById("banner1");
+
+// de notif script
+
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    function timeForBanner() {
+      document.getElementById("banner1").style.display = "block";
+      document.getElementById("banner1").classList.add("slide-down");
+      document.getElementById("banner2").style.display = "block";
+      document.getElementById("banner2").classList.add("slide-down");
+    }
+
+    const timeFilter = new Date("May 17, 2022 01:00:00 AM").getTime(),
+      currentTime = new Date().getTime(),
+      subtractMilliSecondsValue = timeFilter - currentTime;
+    setTimeout(timeForBanner, subtractMilliSecondsValue);
+  },
+  false
+);
